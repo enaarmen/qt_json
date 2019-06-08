80918bc88e70d593e3e90dcd727548ca5accb370
yves adonias <yves.adonias@epitech.eu>
2019-06-08 11:48:37 +0200

    je commence a recuperer les infos case par case pour faire la verification du login et mot de passe.

--- jsonqtnotes.cpp
+++ jsonqtnotes.cpp
@@ -5,6 +5,7 @@
 #include <QtNetwork/QNetworkReply>
 #include <QJsonObject>
 #include <QJsonValue>
+#include <QJsonDocument>
 
 #include <QDebug>
 
@@ -25,39 +26,49 @@ jsonQtNotes::~jsonQtNotes()
 
 void jsonQtNotes::ConnectOrthoJsonByAllOtrhos(QString email, QString password)
 {
-    QJsonObject json;
+    //QJsonObject json;
     QNetworkRequest request(QUrl(QString(BASE_URL) + QString("/get_all_ortho")));
     QNetworkAccessManager *q_network_manager = new QNetworkAccessManager(this);
     QNetworkReply *reply;// = q_network_manager.get(request);
 
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
     reply = q_network_manager->get(request);
-    //connect(manager, &QNetworkAccessManager::finished, this, &QNetworkReply::isFinished);
-    //json.insert("email", email);
-    //json.insert("password", password);
-    //reply = q_network_manager->get(request);
-    //reply = q_network_manager.post(request, QJsonDocument(json).toJson());
+
     while (!reply->isFinished())
         qApp->processEvents();
     QString response_data = reply->readAll();
     QJsonDocument json_document = QJsonDocument::fromJson(response_data.toUtf8());
-    json = json_document.object();
-    QJsonValue value = json.value(QString("password"));
-    qDebug() << "valeur du password :" + value.toString();
-    qDebug() << "reply readAll: " << reply->readAll();
-    qDebug() << "response_data is:" << response_data;
-    qDebug() << "Json got: " << json_document.toJson();
+
+    //qDebug() << "is empty:" << json_document[0]["_id"].toString();
+
     ui->connection_get->setText(json_document.toJson());
+    if (CheckLogin(json_document, email, password)) {
+        qDebug() << "got:" << email << "connected";
+    } else
+        qDebug() << "bad connection";
     reply->deleteLater();
 }
 
 void jsonQtNotes::on_connection_clicked()
 {
-    if (!ui->email->find("email") && !ui->email->size().isEmpty()
-            && !ui->password->find("password")
-            && !ui->password->size().isEmpty()) {
-        ConnectOrthoJsonByAllOtrhos(ui->email->toPlainText(), ui->password->toPlainText().toUtf8());
-        qDebug() << QString("recuperation des infos email: ") + ui->email->toPlainText().toUtf8();
-    } else
-        qDebug() <<"bad connection";
+    qDebug() << "recuperation des infos email: " << ui->email->toPlainText();
+    ConnectOrthoJsonByAllOtrhos(ui->email->toPlainText(), ui->password->toPlainText());
+}
+
+bool jsonQtNotes::CheckLogin(QJsonDocument & json, QString email, QString password)
+{
+    int count = 0;
+
+    for (auto it = json.array().begin(); it != json.array().end(); ++it) {
+        qDebug() << "iterateur:" << (*it);
+        QJsonObject json = (*it).toObject();
+        if (!json.value("email").toString().compare(email) && !json.value("password").toString().compare(password)) {
+            this->id = json["_id"].toString();
+            this->email = json["email"].toString();
+            qDebug() << "dans le if";
+            return (true);
+        }
+        ++count;
+    }
+    return(false);
 }
--- jsonqtnotes.h
+++ jsonqtnotes.h
@@ -1,6 +1,8 @@
 #ifndef JSONQTNOTES_H
 #define JSONQTNOTES_H
 
+#include <QJsonDocument>
+#include <QString>
 #include <QtCore>
 #include <QWidget>
 
@@ -17,14 +19,16 @@ class jsonQtNotes : public QWidget
 public:
     explicit jsonQtNotes(QWidget *parent = nullptr);
     ~jsonQtNotes();
-    void ConnectOrthoJsonByAllOtrhos(QString email, QString password);
+    void ConnectOrthoJsonByAllOtrhos(QString email, QString password); //deprecated before email
+    bool CheckLogin(QJsonDocument & json, QString email, QString password);
 
 private slots:
     void on_connection_clicked();
 
 private:
     Ui::jsonQtNotes *ui;
-
+    QString         id;
+    QString         email;
 };
 
 #endif // JSONQTNOTES_H
